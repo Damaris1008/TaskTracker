805ce647e23e592ac42a59bfb76d0d59f0d1d24c
Damaris1008 <damarisgomezse@gmail.com>
2023-04-23 23:01:15 +0200

    feat(develop): Priority and status added to card attributes

--- src/routes/cards.routes.js
+++ src/routes/cards.routes.js
@@ -6,7 +6,6 @@ const router = Router();
 // Get all cards
 router.get("/cards", async (req,  res) => {
     const cards = await Card.find().sort({createdAt: 'desc'}).lean();
-    console.log(cards);
     res.render("cards/all-cards", { cards });
 });
 
@@ -18,25 +17,33 @@ router.get("/cards/add", (req, res) => {
 // Post a new card
 router.post("/cards/add", async (req, res) => {
 
-    const { title, description, status } = req.body;
+    var { title, description, status, priority } = req.body;
     const errors = [];
+    if(priority==""){
+        priority=null;
+    }
+    if(status==""){
+        status=null;
+    }
     if (!title) {
-        errors.push({ text: "Debe introducir un título" });
+        errors.push({ text: "Debe introducir un título." });
     }
-    if (!status) {
-        errors.push({ text: "Debe asignar un estado" });
+    if (!status || (status != "Por hacer" && status != "En progreso" && status != "Terminado")) {
+        errors.push({ text: "Debe asignar uno de los tres estados posibles."});
     }
+
     if (errors.length > 0) {
         res.render("cards/new-card", {
             errors,
             title,
             description,
-            status
+            status,
+            priority
         });
     } else {
-        const newCard = new Card({ title, description, status });
+        const newCard = new Card({ title, description, status, priority });
         await newCard.save();
-        req.flash("success_msg", "Tarjeta agregada correctamente");
+        req.flash("success_msg", "Tarjeta agregada correctamente.");
         res.redirect("/cards");
     }
 });
@@ -45,32 +52,42 @@ router.post("/cards/add", async (req, res) => {
 router.get("/cards/edit/:id", async (req, res) => {
     const card = await Card.findById(req.params.id).lean();
     const id = req.params.id;
-    const { title, description, status } = card;
-    res.render("cards/edit-card", { id, title, description, status });
+    const { title, description, status, priority } = card;
+    res.render("cards/edit-card", { id, title, description, status, priority });
 });
 
 // Update a card
 router.put("/cards/edit/:id", async (req, res) => {
-    var { title, description, status } = req.body;
+    var { title, description, status, priority } = req.body;
     const id = req.params.id;
     const errors = [];
+    if(status==""){
+        status=null;
+    }
+    if(priority==""){
+        priority=null;
+    }
     if (!title) {
-        errors.push({ text: "Debe introducir un título" });
+        errors.push({ text: "Debe introducir un título." });
     }
-    if (!status) {
-        errors.push({ text: "Debe asignar un estado" });
+    if (!status || (status != "Por hacer" && status != "En progreso" && status != "Terminado")) {
+        errors.push({ text: "Debe asignar uno de los tres estados posibles." });
     }
+
     if (errors.length > 0) {
+        console.log(status);
         res.render("cards/edit-card", {
             errors,
             id,
             title,
             description,
-            status
+            status, 
+            priority
         });
     } else {
-        await Card.findByIdAndUpdate(req.params.id, { title, description, status });
-        req.flash("success_msg", "Tarjeta actualizada correctamente");
+
+        await Card.findByIdAndUpdate(req.params.id, { title, description, status, priority });
+        req.flash("success_msg", "Tarjeta actualizada correctamente.");
         res.redirect("/cards");
     }
 });
@@ -78,7 +95,7 @@ router.put("/cards/edit/:id", async (req, res) => {
 // Delete a card
 router.delete("/cards/delete/:id", async (req, res) => {
     await Card.findByIdAndDelete(req.params.id);
-    req.flash("success_msg", "Tarjeta eliminada correctamente");
+    req.flash("success_msg", "Tarjeta eliminada correctamente.");
     res.redirect("/cards");
 });
 
