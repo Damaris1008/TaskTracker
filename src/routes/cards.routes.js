4c289ceadb4b1205852d9c6ed91887b6a3c5c2ef
Damaris1008 <damarisgomezse@gmail.com>
2023-04-23 18:17:37 +0200

    feat(develop): Update card operation

--- src/routes/cards.routes.js
+++ src/routes/cards.routes.js
@@ -3,16 +3,19 @@ import Card from "../models/Card";
 
 const router = Router();
 
+// Get all cards
 router.get("/cards", async (req,  res) => {
     const cards = await Card.find().sort({createdAt: 'desc'}).lean();
     console.log(cards);
     res.render("cards/all-cards", { cards });
 });
 
+// Get form for adding a card
 router.get("/cards/add", (req, res) => {
     res.render("cards/new-card");
 });
 
+// Post a new card
 router.post("/cards/add", async (req, res) => {
 
     const { title, description, status } = req.body;
@@ -37,7 +40,17 @@ router.post("/cards/add", async (req, res) => {
     }
 });
 
+// Get form for updating a card
+router.get("/cards/edit/:id", async (req, res) => {
+    const card = await Card.findById(req.params.id).lean();
+    res.render("cards/edit-card", { card });
+});
 
-
+// Update a card
+router.put("/cards/edit/:id", async (req, res) => {
+    const { title, description, status } = req.body;
+    await Card.findByIdAndUpdate(req.params.id, { title, description, status });
+    res.redirect("/cards");
+});
 
 export default router;
